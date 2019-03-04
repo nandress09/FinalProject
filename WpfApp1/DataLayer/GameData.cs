b31cb79c582f7c40b19c54623f3955601b331cce
nandress09 <andres23@mail.nmc.edu>
2019-03-03 20:25:22 -0500

    Add project files.

---
+++ WpfApp1/DataLayer/GameData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.DataLayer
+{
+    class GameData
+    {
+        public static Player PlayerData()
+        {
+            return new Player()
+            {
+                Id = 1,
+                Name = "Eiic",
+                Age = 43,
+                JobTitle = Player.JobTitleName.Explorer,
+                Race = Character.RaceType.Human,
+                Health = 100,
+                Lives = 3,
+                ExperiencePoints = 0,
+                LocationId = 0
+            };
+        }
+
+        public static List<string> InitialMessages()
+        {
+            return new List<string>()
+            {
+                "\tYou have been hired by the Norlon Corporation to participate in its latest endeavor, the Aion Project. Your mission is to  test the limits of the new Aion Engine and report back to the Norlon Corporation.",
+                "\tYou will begin by choosing a new location and using Aion Engine to travel to that point in the Galaxy.\n\tThe Aion Engine, design by Dr. Tormeld, is limited to four slipstreams, denoted by the first four Greek letters, from any given locations."
+            };
+        }
+    }
+}
---
+++ WpfApp1/PresentationLayer/GameSessionViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.PresentationLayer
+{
+    public class GameSessionViewModel
+    {
+        private List<string> _messages;
+        private DateTime _gameStartTime;
+        private Player _player;
+       
+
+        public Player Player
+        {
+            get { return _player; }
+            set { _player = value; }
+        }
+
+        public string MessageDisplay
+        {
+            get { return FormatMessagesForViewer(); }
+        }
+
+        public GameSessionViewModel()
+        {
+
+        }
+
+        public GameSessionViewModel (
+            Player player, 
+            List<string> initialMessages)
+        {
+            _player = player;
+            _messages = initialMessages;
+            InitializeView();
+        }
+
+        private void InitializeView()
+        {
+            _gameStartTime = DateTime.Now;
+        }
+
+        private string FormatMessagesForViewer()
+        {
+            List<string> lifeMessages = new List<string>();
+
+            for (int index = 0; index < _messages.Count; index++)
+            {
+                lifeMessages.Add($" <T:{GameTime().ToString(@"hh\:mm\:ss")} " + _messages[index]);
+            }
+
+            lifeMessages.Reverse();
+
+            return string.Join("\n\n", lifeMessages);
+        }
+
+        private TimeSpan GameTime()
+        {
+            return DateTime.Now - _gameStartTime;
+        }
+
+
+    }
+}
